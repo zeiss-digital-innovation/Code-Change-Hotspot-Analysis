2f722fbcdc2b397557f21c42ffb16c95d36047e0
JesusIsKing <DITSTEIN@DI05N000C9>
2025-03-04 16:36:20 +0100

    Treemap counting a weight of zero as one
    And I added Documentation for treemap.py:
    
    The script adds one "change" towards the changes of a file,
    counting it as the "initial commit",
    which are not represented in the data seen in the treemap
    because file names from the older data set that are not in the newer data set will be added with a weight of "zero".
    Since "plotly.express" does not show paths with a value of zero, a "initial commit" has to be added.

--- doc/DOCUMENTATION.md
+++ doc/DOCUMENTATION.md
@@ -6,3 +6,6 @@ git log --before="2024-01-01" --pretty=format:"" --name-only`
 
 git log --since="2024-01-01" --pretty=format:"" --name-only
 ```
+
+## treemap.py
+The script adds one "change" towards the changes of a file, counting it as the "initial commit", which are not represented in the data seen in the treemap because file names from the older data set that are not in the newer data set will be added with a weight of "zero". Since "plotly.express" does not show paths with a value of zero, a "initial commit" has to be added.
--- src/treemap.py
+++ src/treemap.py
@@ -3,15 +3,17 @@ import plotly.express as px
 
 # Schritt 1: Lese die Daten aus der Textdatei
 data = []
-file_path:str = "c:/Users/DITSTEIN/OneDrive - Carl Zeiss AG/Dokumente/[01] Arbeit/[01] Coding/[02] HotSpot Analyse/Code-Change-Hotspot-Analysis/test_daten.txt"
+file_path:str = r"C:\Users\DITSTEIN\OneDrive - Carl Zeiss AG\Dokumente\[01] Arbeit\[01] Coding\[02] HotSpot Analyse\Data\test_data_for_treemap.txt"
 with open(file_path, 'r') as file:
     for line in file:
         path, changes = line.strip().split(': ')
-        data.append({'File Path': path, 'Changes': int(changes)})
+        changes = int(changes)  
+        changes += 1 # Counting this as the initial commit 
+        data.append({'File Path': path, 'Changes': changes})
 
 # Schritt 2: Erstelle ein DataFrame
 df = pd.DataFrame(data)
-
+print(df)
 # Schritt 3: Erstelle die Treemap
 fig = px.treemap(df, 
                  path=['File Path'], 
