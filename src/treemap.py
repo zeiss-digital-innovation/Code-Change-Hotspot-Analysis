4f494d71c0c5618c4ac6bd4db02cc6a98fcdac2f
JesusIsKing <DITSTEIN@DI05N000C9>
2025-03-05 16:36:17 +0100

    Script now checks if the line is empty

--- src/treemap.py
+++ src/treemap.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import plotly.express as px
 
-# Schritt 1: Lese die Daten aus der Textdatei
+
 data = []
-file_path:str = r"C:\Users\DITSTEIN\OneDrive - Carl Zeiss AG\Dokumente\[01] Arbeit\[01] Coding\[02] HotSpot Analyse\Data\test_data_for_treemap.txt"
+file_path:str = r"C:\Users\DITSTEIN\OneDrive - Carl Zeiss AG\Dokumente\[01] Arbeit\[01] Coding\[02] HotSpot Analyse\Data\copy_data_newer_2024.txt"
 with open(file_path, 'r') as file:
     for line in file:
-        path, changes = line.strip().split(': ')
-        changes = int(changes)  
-        changes += 1 # Counting this as the initial commit 
-        data.append({'File Path': path, 'Changes': changes})
+        if line.strip():
+            path, changes = line.strip().split(': ')
+            changes = int(changes)  
+            changes += 1 # Counting this as the initial commit 
+            data.append({'File Path': path, 'Changes': changes})
+        else:
+            continue
 
-# Schritt 2: Erstelle ein DataFrame
+
 df = pd.DataFrame(data)
 print(df)
-# Schritt 3: Erstelle die Treemap
+
 fig = px.treemap(df, 
                  path=['File Path'], 
                  values='Changes', 
                  title='Treemap der Dateipfade basierend auf Änderungen')
 
-# Schritt 4: Zeige die Treemap an
+
 fig.show()
