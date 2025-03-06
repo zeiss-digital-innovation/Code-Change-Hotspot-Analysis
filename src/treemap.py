576eead909066d8019699fe26d2f272215f8e81e
JesusIsKing <DITSTEIN@DI05N000C9>
2025-03-06 23:07:37 +0100

    Moved functions from other python files
    Making the other two python files absolete.

--- src/treemap.py
+++ src/treemap.py
@@ -8,8 +8,76 @@ import shutil
 import re
 from collections import Counter 
 
+def count_lines(old_data_file_path: str, new_data_file_path: str):
+
+    with open(old_data_file_path, 'r') as file:
+            # reads all lines and removes empty spaces and line breaks
+            old_data_lines = [line.strip() for line in file.readlines() if line.strip()] 
+            # Filers Special characters and keep only alphanumeric character
+            old_data_filtered_lines = [re.sub(r'[^a-zA-Z0-9\s\\\/\.]', '', line) for line in old_data_lines]
+            # Counts the occurences of every filtered line
+            old_data_line_counts = Counter(old_data_filtered_lines)
+            
+            old_data_output_filename = "older_counted.txt"
+
+            with open(old_data_output_filename, 'w') as output_file:
+                for line, count in old_data_line_counts.items():
+                    if line:  # Ensure the line is not empty
+                        output_file.write(f"{line}: {count}\n")
+
+    with open(new_data_file_path, 'r') as file:
+        # reads all lines and removes empty spaces and line breaks
+        new_data_lines = [line.strip() for line in file.readlines() if line.strip()] 
+        # Filers Special characters and keep only alphanumeric character
+        new_data_filtered_lines = [re.sub(r'[^a-zA-Z0-9\s\\\/\.]', '', line) for line in new_data_lines]
+        # Counts the occurences of every filtered line
+        new_data_line_counts = Counter(new_data_filtered_lines)
+            
+        new_data_output_filename = "newer_counted.txt"
+
+        with open(new_data_output_filename, 'w') as output_file:
+            for line, count in new_data_line_counts.items():
+                if line:  # Ensure the line is not empty
+                    output_file.write(f"{line}: {count}\n")
+
+    file_names: list[str] = []    
+    file_names.append(old_data_output_filename)
+    file_names.append(new_data_output_filename)
+    
+    return file_names
 
 
+def compare_data(file_names: list[str]):
+    # Creates absolute paths for each file, for better functionality
+    script_dir: str = os.path.dirname(os.path.abspath(__file__))
+
+    older_data_counted_file_path: str = os.path.join(script_dir, file_names[0])
+    newer_data_counted_file_path: str = os.path.join(script_dir, file_names[1])
+    
+    shutil.copyfile(newer_data_counted_file_path, "treemap_data.txt")
+
+    treemap_data_file_path: str = os.path.join(script_dir, "treemap.txt")
+    
+    # Creates the lists so only the paths are compared
+    with open(older_data_counted_file_path, 'r') as file:
+        older_data_paths_as_list: list[str] = []
+        for line in file: 
+            path, _ = line.strip().split(": ")
+            older_data_paths_as_list.append(path)
+
+    with open(newer_data_counted_file_path, 'r') as file2:
+        newer_data_paths_as_list: list[str] = []
+        for line in file2.readlines():
+            path, _ = line.strip().split(": ")
+            newer_data_paths_as_list.append(path)
+    
+    # Actual Comparison
+    with open(treemap_data_file_path, 'a+') as file3: 
+        for line in older_data_paths_as_list:
+            if line not in newer_data_paths_as_list:
+                file3.write(f"\n{line}: 0")
+    
+    
 
 
 def displaying_treemap(treemap_data_file_path: str):
@@ -44,7 +112,6 @@ def displaying_treemap(treemap_data_file_path: str):
 
 
     fig.show()
-
 
 
 if len(sys.argv) != 3:
