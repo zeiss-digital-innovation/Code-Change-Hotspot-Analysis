a1f6dbaf27ef71a6a20ae4ce88d16572584a7864
JesusIsKing <DITSTEIN@DI05N000C9>
2025-03-14 08:55:29 +0100

    Added test for second count_lines failure path

--- src/tests/test_function.py
+++ src/tests/test_function.py
@@ -1,11 +1,22 @@
-import createHotspots as ch
+import sys
 import unittest
+
+sys.dont_write_bytecode = True
+
+import src.createHotspots as ch
+
 
 class TestFunctions(unittest.TestCase):
     
-    def test_count_lines_faliure(self):
+    def test_count_lines_faliure_one(self):
         with self.assertRaises(SystemExit) as cm:
             ch.count_lines("src/tests/data/old_empty.txt", "src/tests/data/new.txt")
+
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_count_lines_faliure_two(self):
+        with self.assertRaises(SystemExit) as cm:
+            ch.count_lines("src/tests/data/old.txt", "src/tests/data/new_empty.txt")
 
         self.assertEqual(cm.exception.code, 1)
     
