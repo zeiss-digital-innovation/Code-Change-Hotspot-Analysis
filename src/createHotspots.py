61366a8d17637bab647096b373b8225e900c5465
JesusIsKing <DITSTEIN@DI05N000C9>
2025-03-27 16:43:21 +0100

    Fixed f - string bug
    f-string used with double quotes (" ") cannot contain another pair of double quotes
    changed them to single quotes (' ')

--- src/createHotspots.py
+++ src/createHotspots.py
@@ -227,7 +227,7 @@ def script():
     # because it saves more time if treemap data actually exists
     if check_if_data_exists("treemap_data.txt"):
         print(
-            f"Found path to treemap data:\n\n{create_path_to_data("treemap_data.txt")}\n\n"
+            f"Found path to treemap data:\n\n{create_path_to_data('treemap_data.txt')}\n\n"
             "Skipping 3/3 steps..."
         )
         displaying_treemap(
@@ -238,8 +238,8 @@ def script():
         "older_counted.txt"
     ):
         print(
-            f"Found path to newer_counted data:\n\n{create_path_to_data("newer_counted.txt")}\n\n"
-            f"And found path to older_counted data:\n\n{create_path_to_data("older_counted.txt")}\n\n"
+            f"Found path to newer_counted data:\n\n{create_path_to_data('newer_counted.txt')}\n\n"
+            f"And found path to older_counted data:\n\n{create_path_to_data('older_counted.txt')}\n\n"
             "Skipping 2/3 steps..."
         )
         treemap_data_file_path: str = compare_data(
@@ -250,8 +250,8 @@ def script():
 
     elif check_if_data_exists("new.txt") and check_if_data_exists("old.txt"):
         print(
-            f"Found path to new data:\n\n{create_path_to_data("new.txt")}\n\n"
-            f"And found path to old data:\n\n{create_path_to_data("old.txt")}\n\n"
+            f"Found path to new data:\n\n{create_path_to_data('new.txt')}\n\n"
+            f"And found path to old data:\n\n{create_path_to_data('old.txt')}\n\n"
             "Skipping 1/3 steps..."
         )
         older_data_counted_file_path, newer_data_counted_file_path = count_lines(
